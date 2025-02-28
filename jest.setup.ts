7fb50fd563c7a4c54921769d5179a4c869da193a
Manuel-M-M <mmoragamolina@gmail.com>
2025-02-28 13:19:07 +0100

    chore(setup): configure testing and install dependencies
    - Added  for better snapshot testing with styled-components.
    - Installed  for navigation.
    - Updated  to properly mock fetch and fix TextEncoder issue.
    - Ensured  is defined in the test environment.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,18 @@
+import "jest-styled-components";
 import fetchMock from "jest-fetch-mock";
+import { TextEncoder, TextDecoder } from "util";
 
 fetchMock.enableMocks();
+
+global.TextEncoder = TextEncoder;
+
+if (typeof global.TextDecoder === "undefined") {
+  global.TextDecoder = TextDecoder as never;
+}
+
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+} as never;
